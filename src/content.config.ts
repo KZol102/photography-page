527c19acd793ecc90e6808aa7f801f6996f01383
Zoltán Kiss <kzoltanbela@gmail.com>
2026-09-05 00:54:13 +0200

    Started adding blog post support

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,10 @@
 import { defineCollection } from 'astro:content';
 import { photoLoader } from './content/photo-loader';
 import { z } from 'astro/zod';
+import { glob } from 'astro/loaders';
+
+export const blogCategories = ["camera", "lens", "film", "other"] as const;
+export const lensMounts = ["Minolta SR", "M42"] as const;
 
 const film = z.union(
   [
@@ -13,12 +17,12 @@ const film = z.union(
       cross_process: z.string().optional(),
       misc: z.string().optional()
     }
-  )
-])
+    )
+  ])
 
 const photos = defineCollection({
   loader: photoLoader('./src/photos'),
-  schema: ({image}) => z.object({
+  schema: ({ image }) => z.object({
     order: z.int().positive(),
     title: z.string(),
     description: z.string().optional(),
@@ -31,4 +35,19 @@ const photos = defineCollection({
   }),
 });
 
-export const collections = { photos };
+const blog = defineCollection({
+  loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/blog" }),
+  schema: ({ image }) => z.object({
+    title: z.string(),
+    hero: image().optional(),
+    category: z.enum(blogCategories),
+    pubDate: z.date().optional(),
+    lastUpdateDate: z.date().optional(),
+    camera: z.string().optional(),
+    lens: z.string().optional(),
+    mount: z.enum(lensMounts).optional(),
+    film: z.string().optional()
+  }),
+});
+
+export const collections = { photos, blog };
